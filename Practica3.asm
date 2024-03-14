099976939c533295db96cd01d360876630f775f9
Alejandro Perez <2995019300101@ingenieria.usac.edu.gt>
2024-03-13 22:43:38 -0600

    tablero

--- Practica3.asm
+++ Practica3.asm
@@ -1,124 +1,219 @@
-.model medium
-.stack 100h
+;MACROS
+LimpiarConsola MACRO
+    MOV AX, 03h
+    INT 10h
+ENDM
 
-.data
-    ; Mensaje informativo inicial
-    mensaje   db 'UNIVERSIDAD DE SAN CARLOS DE GUATEMALA', 0Dh, 0Ah
-              db 'FACULTAD DE INGENIERIA', 0Dh, 0Ah
-              db 'ESCUELA DE CIENCIAS Y SISTEMAS', 0Dh, 0Ah
-              db 'ARQUITECTURA DE COMPUTADORES Y ENSAMBLADORES 1', 0Dh, 0Ah
-              db 'SECCION A', 0Dh, 0Ah
-              db 'Primer semestre 2024', 0Dh, 0Ah
-              db 'Josue Alejandro Perez Benito', 0Dh, 0Ah
-              db '201712602', 0Dh, 0Ah
-              db 'Practica 3', 0Dh, 0Ah, '$'
-    ; Menu de opciones
-    menuMsg   db 'MENU:', 0Dh, 0Ah
-              db '1. NUEVO JUEGO', 0Dh, 0Ah
-              db '2. PUNTAJES', 0Dh, 0Ah
-              db '3. REPORTES', 0Dh, 0Ah
-              db '4. SALIR', 0Dh, 0Ah
-              db 'Seleccione una opcion: $', 0Dh, 0Ah
-    ; Tablero
-    tablero   db 0Ah, '  |  A  |  B  |  C  |  D  |  E  |  F  |  G  |  H  |' ,0Ah
-              db '  |-----|-----|-----|-----|-----|-----|-----|-----|',  0Ah
-              db '1 |  t  |  c  |  a  |  r  |  *  |  a  |  c  |  t  |',  0Ah
-              db '2 |  p  |  p  |  p  |  p  |  p  |  p  |  p  |  p  |',  0Ah
-              db '3 |     |     |     |     |     |     |     |     |',  0Ah
-              db '4 |     |     |     |     |     |     |     |     |',  0Ah
-              db '5 |     |     |     |     |     |     |     |     |',  0Ah
-              db '6 |     |     |     |     |     |     |     |     |',  0Ah
-              db '7 |  P  |  P  |  P  |  P  |  P  |  P  |  P  |  P  |',  0Ah
-              db '8 |  T  |  C  |  A  |  R  |  *  |  A  |  C  |  T  |',  0Ah
-              db '  |-----|-----|-----|-----|-----|-----|-----|-----|',  0Ah
-              db '  |  A  |  B  |  C  |  D  |  E  |  F  |  G  |  H  |',  0Ah, '$'
+ImprimirCadenas MACRO registroPrint
+    MOV AH, 09h
+    LEA DX, registroPrint
+    INT 21h
+ENDM
 
-    ; Lectura de entradas
-    opcion    db ?
-    ; Validacion de entradas
-    error_msg db 0Dh, 0Ah, 'Opcion no valida. Intente de nuevo.', 0Dh, 0Ah, '$'
-    valid_msg db 0Dh, 0Ah, 'Opcion valida.', 0Dh, 0Ah, '$'
+ObtenerOpcion MACRO regOpcion
+    MOV AH, 01h
+    INT 21h
+    MOV regOpcion, AL
+ENDM
+
+ImprimirTableroJuego MACRO
+    LOCAL fila, columna
+
+    MOV BX, 0 ; Indice del indicador de filas -> Inicia en 0
+    XOR SI, SI ; Indice para el tablero -> Inicia en 0
+
+    ImprimirCadenas indicadorColumnas
+    MOV CL, 0 ; Contador de columnas
+
+    fila:
+        ImprimirCadenas salto
+        MOV AH, 02h ; Codigo Interrupcion para imprimir un caracter
+        MOV DL, indicadorFilas[BX] ; Caracter a imprimir
+        INT 21h ; Llamar Interrupcion
+
+        MOV DL, 32 ; Caracter del espacio en blanco
+        INT 21h
+
+        columna:
+            MOV DL, tablero[SI] ; Caracter del tablero
+            INT 21h
+
+            MOV DL, 124 ; Caracter |
+            INT 21h
+
+            INC CL ; Incrementamos CL en 1 -> CL++
+            INC SI ; Incrementamos SI en 1 -> SI++
+
+            CMP CL, 8 ; Si no ha pasado por las 8 columnas que regrese a la etiqueta 'columna'
+            JB columna ; Salto a columna
+
+            MOV CL, 0 ; Reiniciar Contador de columnas
+            INC BX ; Incrementar indice indicador de filas -> BX++
+            
+            CMP BX, 8 ; Si no ha pasado por todas las filas que regrese a la etiqueta 'fila'
+            JB fila
+ENDM
+
+LlenarTablero MACRO
+    LOCAL llenarPeon1, llenarPeon2, Piezas1, Piezas2
+
+    MOV SI, 0 ; Indice del tablero
+    MOV CH, 0 ; Contador de peones
+
+    Piezas1:
+        MOV DX, 116 ; Caracter a guardar en el tablero
+        MOV tablero[SI], DL ; Escribir caracter en el tablero
+        PUSH DX ; Guardamos el registro en la pila
+        INC SI ; Incrementamos indice de tablero -> SI++
+
+        MOV DX, 99
+        MOV tablero[SI], DL
+        PUSH DX
+        INC SI
+
+        MOV DX, 97
+        MOV tablero[SI], DL
+        PUSH DX
+        INC SI
+
+        MOV DX, 114
+        MOV tablero[SI], DL
+        INC SI
+
+        MOV DX, 35
+        MOV tablero[SI], DL
+        INC SI
+
+        POP DX ; Extraemos registro de la pila y lo almacenamos en DX
+        MOV tablero[SI], DL ; Escribimos caracter en el tablero
+        INC SI ; Incrementamos indice de tablero -> SI++
+
+        POP DX
+        MOV tablero[SI], DL
+        INC SI
+
+        POP DX
+        MOV tablero[SI], DL
+        INC SI
     
-    msj_vs    db 0Ah,' VS ', 0Ah, '$'
-    msj_IA    db ' IA ', 0Ah, '$'
+    llenarPeon1:
+        MOV tablero[SI], 112 ; Escribir caracter en tablero
+        INC SI ; Incrementamos indice del tablero
+        INC CH ; Incrementamos contador de peones
 
-.code
-main PROC
-                 mov ax, @data
-                 mov ds, ax
+        CMP CH, 8 ; Si es menor a 8 que regrese a la etiqueta 'llenarPeon1', caso contrario continua
+        JB llenarPeon1
 
-    ; /* -------------------------------------------------------------------------- */
-    ; /*                               MENSAJE INICIAL                              */
-    ; /* -------------------------------------------------------------------------- */
-                 mov ah, 9
-                 mov dx, OFFSET mensaje
-                 int 21h
-    ; /* -------------------------------------------------------------------------- */
-    ; /*                               MENU PRINCIPAL                               */
-    ; /* -------------------------------------------------------------------------- */
-    menuLoop:    
-    ; Imprimir el menú
-                 mov ah, 9
-                 mov dx, OFFSET menuMsg
-                 int 21h
+        MOV CH, 0
+        MOV SI, 48
 
-    ; Leer la opción ingresada por el usuario
-                 mov ah, 1
-                 int 21h
+    llenarPeon2:
+        MOV tablero[SI], 80
+        INC SI
+        INC CH
 
-    ; Opciones
-                 cmp al, '1'
-                 je  new_game
-                 cmp al, '2'
-                 je  option_valid
-                 cmp al, '3'
-                 je  option_valid
-                 cmp al, '4'
-                 je  exit
+        CMP CH, 8
+        JB llenarPeon2
 
-    ; Si no es válida, mostrar mensaje de error
-                 mov ah, 9
-                 mov dx, OFFSET error_msg
-                 int 21h
-    ; Volver al menú
-                 jmp menuLoop
+    Piezas2:
+        MOV DX, 84
+        MOV tablero[SI], DL
+        PUSH DX
+        INC SI
 
-    option_valid:
-    ; Si la opción es válida, mostrar mensaje de opción válida
-                 mov ah, 9
-                 mov dx, OFFSET valid_msg
-                 int 21h
-    ; Salto al menú principal
-                 jmp menuLoop
-    ;/* -------------------------------------------------------------------------- */
-    ;/*                                 NUEVO JUEGO                                */
-    ;/* -------------------------------------------------------------------------- */
-    new_game:    
+        MOV DX, 67
+        MOV tablero[SI], DL
+        PUSH DX
+        INC SI
 
-    ; Mostrar el mensaje 'VS'
-                 mov ah, 9
-                 mov dx, OFFSET msj_vs
-                 int 21h
+        MOV DX, 65
+        MOV tablero[SI], DL
+        PUSH DX
+        INC SI
 
-    ; Mostrar el mensaje 'IA'
-                 mov ah, 9
-                 mov dx, OFFSET msj_IA
-                 int 21h
+        MOV DX, 82
+        MOV tablero[SI], DL
+        INC SI
 
-    ; Mostrar el tablero
-                 mov ah, 9
-                 mov dx, OFFSET tablero
-                 int 21h
+        MOV DX, 42
+        MOV tablero[SI], DL
+        INC SI
 
-                 jmp menuLoop
+        POP DX
+        MOV tablero[SI], DL
+        INC SI
 
-    ;/* -------------------------------------------------------------------------- */
-    ;/*                                    SALIR                                   */
-    ;/* -------------------------------------------------------------------------- */
-    exit:        
-                 mov ah, 4Ch                  ; Terminar el programa
-                 int 21h
+        POP DX
+        MOV tablero[SI], DL
+        INC SI
 
-main ENDP
+        POP DX
+        MOV tablero[SI], DL
+        INC SI
+ENDM
+.MODEL small
+.STACK 64h
 
-END main
+.DATA
+    salto db 10,13,"$"
+    ;Las cadenas van en el segmento de data
+    ;Al final de las cadenas colocar $ para indicar el final
+    ; Mensaje informativo inicial
+    msj_ini   db 'UNIVERSIDAD DE SAN CARLOS DE GUATEMALA', 10, 13
+              db 'FACULTAD DE INGENIERIA', 10, 13
+              db 'ESCUELA DE CIENCIAS Y SISTEMAS', 10, 13
+              db 'ARQUITECTURA DE COMPUTADORES Y ENSAMBLADORES 1', 10, 13
+              db 'SECCION A', 10, 13
+              db 'Primer semestre 2024', 10, 13
+              db 'Josue Alejandro Perez Benito', 10, 13
+              db '201712602', 10, 13
+              db 'Practica 3', 10, 13 ,'$'
+    ; Menu de opciones
+    msj_menu  db 10,'MENU:', 10, 13
+              db '1. NUEVO JUEGO', 10, 13
+              db '2. PUNTAJES', 10, 13
+              db '3. REPORTES', 10, 13
+              db '4. SALIR', 10, 13
+              db 'Seleccione una opcion: $', 10, 13
+    ; Tablero
+    opcion db 1 dup(32)
+    indicadorColumnas db " A B C D E F G H", "$"
+    indicadorFilas db "12345678", "$"
+    tablero db 64 dup(64);ROW-MAYOR O COLUMN-MAJOR
+
+.CODE
+    MOV AX, @data
+    MOV DS, AX
+    ;Metodo principal (Main)
+    Main PROC
+        LimpiarConsola
+        ImprimirCadenas msj_ini
+        
+        Menu:
+            ImprimirCadenas msj_menu
+            ObtenerOpcion opcion ;Obtener la opcion que elige el usuario
+            LimpiarConsola
+            CMP opcion, 49;Estimulando el registro de banderas 49=1 ASCII
+            JE ImprimirTablero
+            CMP opcion, 50;Estimulando el registro de banderas 50=2 ASCII
+            JE ImprimirPuntajes
+            CMP opcion, 51;Estimulando el registro de banderas 51=3 ASCII
+            JE ImprimirReportes
+            CMP opcion, 52;Estimulando el registro de banderas 52=4 ASCII
+            JE Salir
+
+        ImprimirTablero:
+            LlenarTablero
+            ImprimirTableroJuego
+        
+        ImprimirPuntajes:
+
+        ImprimirReportes:
+
+            JMP Menu
+        Salir:
+            MOV AX, 4C00h ;Interrupcion para terminar el programa
+            INT 21h
+    Main ENDP
+
+END
